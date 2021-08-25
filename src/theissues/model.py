62df29934dde85f8ef9376b8a54c90ff60d68dbf
Garrin McGoldrick <garrin.mcgoldrick@gmail.com>
2021-08-25 11:19:47 -0400

    add transformer model

---
+++ src/theissues/model.py
@@ -0,0 +1,135 @@
+"""
+Transformer language model.
+
+See: https://pytorch.org/tutorials/beginner/transformer_tutorial.html.
+"""
+
+import math
+
+import torch
+
+
+class PositionalEncoding(torch.nn.Module):
+    """
+    Model which adds position encoding to an embedding space.
+
+    The model has no learnable parameters.
+    """
+
+    def __init__(self, ndims: int, max_seq_len: int):
+        """
+        Args:
+            ndims: encoder dimensions
+            max_seq_len: maximum number tokens in a sequence
+        """
+        super().__init__()
+
+        basis = torch.zeros(max_seq_len, ndims)
+        # array with token index at each position
+        position = torch.arange(0, max_seq_len, dtype=torch.float)
+        # prepare to broadcast accross the embedding dimension
+        position = position[:, None]
+        # Later dimensions get progressively longer trig. period, such that the
+        # first dimension oscillates between 1 and -1 at each token, while the
+        # last dimensions increase or decrease steadily.
+        period = -math.log(2 * max_seq_len) / ndims
+        period = torch.exp(torch.arange(0, ndims, 2).float() * period)
+        # build the sin / cos basis
+        basis[:, 0::2] = torch.sin(position * period * math.pi * 0.5)
+        if ndims % 2 != 0:
+            basis[:, 1::2] = torch.cos(position * period[:-1] * math.pi * 0.5)
+        else:
+            basis[:, 1::2] = torch.cos(position * period * math.pi * 0.5)
+        # add batch dimension
+        basis = basis[:, None, :]
+        self.register_buffer("basis", basis)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x + self.basis[: x.size(0)]
+
+
+class TransformerModel(torch.nn.Module):
+    """
+    Model maps a sequence of tokens to a sequence of proabilities over the
+    vocabulary.
+
+    Uses an embedding layer, a transformer and a decoder.
+    """
+
+    def __init__(
+        self,
+        nvocab: int,
+        seq_len: int,
+        ndims_embed: int,
+        ndims_trans: int,
+        nheads: int,
+        nlayers: int,
+        dropout: float,
+    ):
+        """
+        Args:
+            nvocab: number of vocabulary entries
+            seq_len: number of tokens in a sequence
+            ndims_embed: number of hidden dimensions in the embedding (and all
+                token respresentations)
+            ndims_trans: number of hidden dimensions in the transformer
+            nheads: number of attention heads
+            nlayers: number of transfomer layers
+        """
+        super().__init__()
+
+        self.ndims_embed = ndims_embed
+
+        self.positional_encoder = PositionalEncoding(
+            ndims=ndims_embed, max_seq_len=seq_len
+        )
+        self.transformer_encoder = torch.nn.TransformerEncoder(
+            encoder_layer=torch.nn.TransformerEncoderLayer(
+                d_model=ndims_embed,
+                nhead=nheads,
+                dim_feedforward=ndims_trans,
+                dropout=dropout,
+            ),
+            num_layers=nlayers,
+        )
+        self.embeddings = torch.nn.Embedding(nvocab, ndims_embed)
+        self.decoder = torch.nn.Linear(ndims_embed, nvocab)
+        self.dropout = torch.nn.Dropout(dropout)
+
+        self.init_weights()
+
+    @staticmethod
+    def build_subsequent_mask(n: int) -> torch.FloatTensor:
+        """
+        Build an array with upper trinagular elements set to `-inf` and the
+        rest along with diangonal set to `0`.
+
+        With `n` set to a sequence length, this creates a mask which can be
+        used to progressively allow each token in the sequence to propagate
+        to the output.
+        """
+        return torch.triu(torch.ones(n, n) * -math.inf, diagonal=1)
+
+    def init_weights(self, scale: float = 0.1):
+        torch.nn.init.uniform_(self.embeddings.weight, -scale, scale)
+        torch.nn.init.zeros_(self.decoder.weight)
+        torch.nn.init.uniform_(self.decoder.weight, -scale, scale)
+
+    def forward(
+        self, x: torch.LongTensor, src_mask: torch.FloatTensor
+    ) -> torch.FloatTensor:
+        # convert the token indieces to the embedding space, with positional
+        # encodings and dropout applied
+        x = self.embeddings(x) * math.sqrt(self.ndims_embed)
+        x = self.positional_encoder(x)
+        x = self.dropout(x)
+
+        # run through the transformer into the output feature space
+        x = self.transformer_encoder(x, src_mask)
+        # decode the features into token weights
+        x = self.decoder(x)
+
+        # NOTE: `x` is now appropriate for `CrossEntropyLoss`, but to get the
+        # token probabilities, `Softmax` must be applied.
+
+        return x
---
+++ tests/test_model.py
@@ -0,0 +1,73 @@
+import numpy as np
+import torch
+
+from theissues import model
+
+
+def test_position_encoding_basis_has_correct_dims():
+    encoding = model.PositionalEncoding(4, 8)
+    assert encoding.basis.shape == (8, 1, 4)
+
+
+def test_position_encoding_basis_has_increasing_trig_period():
+    encoding = model.PositionalEncoding(4, 8)
+    # the first dimension oscillates at each token index
+    np.testing.assert_allclose(
+        encoding.basis[:, 0, 0], [0, 1, 0, -1, 0, 1, 0, -1], atol=1e-6
+    )
+    # the last dimension is cos with 1/4 cycle so it is decreasing from 1 to
+    # nearly 0
+    assert torch.all(encoding.basis[1:, 0, -1] < encoding.basis[:-1, 0, -1])
+    np.testing.assert_allclose(encoding.basis[-1, 0, -1], -1, rtol=1e-1)
+
+
+def test_position_encoding_basis_has_sin_cos_basis():
+    encoding = model.PositionalEncoding(4, 8)
+    np.testing.assert_allclose(encoding.basis[0, 0, :], [0, 1, 0, 1], atol=1e-6)
+
+
+def test_position_encoding_basis_handles_odd_dims():
+    encoding = model.PositionalEncoding(3, 8)
+    assert encoding.basis.shape == (8, 1, 3)
+    np.testing.assert_allclose(encoding.basis[0, 0, :], [0, 1, 0], atol=1e-6)
+
+
+def test_position_encoding_adds_to_x():
+    # 4 dim tokens, up to 8 tokens
+    encoding = model.PositionalEncoding(4, 8)
+    # batch of 2 tokens
+    x = torch.FloatTensor(
+        [
+            [0, 1, 2, 3],
+            [1, 2, 3, 4],
+        ]
+    )
+    out = encoding(x)
+    # uses encodings for only 2 tokens
+    np.testing.assert_allclose(out, x + encoding.basis[:2, :], atol=1e-6)
+
+
+def test_transformer_model_generates_subsequent_mask():
+    mask = model.TransformerModel.build_subsequent_mask(3)
+    np.testing.assert_allclose(
+        mask, [[0, -np.inf, -np.inf], [0, 0, -np.inf], [0, 0, 0]]
+    )
+
+
+def test_transformer_model_outputs_weights_over_vocab():
+    m = model.TransformerModel(
+        nvocab=32,
+        seq_len=8,
+        ndims_embed=4,
+        ndims_trans=6,
+        nheads=2,
+        nlayers=5,
+        dropout=0,
+    )
+    # 8 tokens, 3 sequences in the batch
+    x = torch.arange(8 * 3, dtype=torch.long).view((8, 3))
+    out = m(x, torch.ones((8, 8), dtype=torch.float))
+    out = out.detach()
+    # prob. over 32 vocab tokens, for 3 sequencs, for each of the 8 input tokens
+    # with the other dimensions having been collected in the transformer
+    assert out.shape == (8, 3, 32)
